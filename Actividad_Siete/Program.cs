b610dda6c4abd5e8186f2484a946e693b9d8a567
Sebas <124101691+SebasDP12@users.noreply.github.com>
2024-07-25 13:24:37 -0600

    termine de hacer las operaciones solo faltarian los cathc

--- Actividad_Siete/Program.cs
+++ Actividad_Siete/Program.cs
@@ -1,4 +1,63 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+ List<int> lista = new List<int>();
+
+while (true)
+{
+    Console.Clear();
+    Menu();
+    Console.Write("Seleccione una opción: ");
+    int opcion = Convert.ToInt32(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 1:
+            Menu();
+            break;
+        case 2:
+            lista = Numeros();
+            break;
+        case 3:
+            if (lista != null && lista.Count > 0)
+            {
+                int moda = CalcularModa(lista);
+                Console.WriteLine($"La moda de la lista es: {moda}");
+            }
+            else
+            {
+                Console.WriteLine("Primero debe ingresar los números.");
+            }
+            break;
+        case 4:
+            if (lista != null && lista.Count > 0)
+            {
+                double mediana = CalcularMediana(lista);
+                Console.WriteLine($"La mediana de la lista es: {mediana}");
+            }
+            else
+            {
+                Console.WriteLine("Primero debe ingresar los números.");
+            }
+            break;
+        case 5:
+            if (lista != null && lista.Count > 0)
+            {
+                double desviacion = CalcularDesviacion(lista);
+                Console.WriteLine($"La desviación estándar de la lista es: {desviacion}");
+            }
+            else
+            {
+                Console.WriteLine("Primero debe ingresar los números.");
+            }
+            break;
+        default:
+            Console.WriteLine("Opción inválida. Intente de nuevo.");
+            break;
+    }
+    Console.WriteLine("\nPresiona Enter para continuar...");
+    Console.ReadLine();
+}
 
 static void Menu()
 {
@@ -6,20 +65,18 @@ static void Menu()
     Console.WriteLine("2. Ingresar Numeros");
     Console.WriteLine("3. Calcular y mostrar moda");
     Console.WriteLine("4. Calcular y mostrar la mediana");
-    Console.WriteLine("5. Calcular y mostrar la desivacion mediana");
-    Console.WriteLine("Volver a ingresar los numeros");
+    Console.WriteLine("5. Calcular y mostrar la desviación estándar");
 }
 
 static List<int> Numeros()
 {
-    Console.WriteLine("Ingrese el limite de la lista: ");
+    Console.Write("Ingrese el límite de la lista: ");
     int limite = Convert.ToInt32(Console.ReadLine());
 
     List<int> lista = new List<int>();
     for (int i = 0; i < limite; i++)
     {
-        Console.WriteLine("Ingrese los valores: "+ i+1);
-        
+        Console.Write($"Ingrese el valor {i + 1}: ");
         int valor = Convert.ToInt32(Console.ReadLine());
         lista.Add(valor);
     }
@@ -30,26 +87,21 @@ static int CalcularModa(List<int> lista)
 {
     lista.Sort();
 
-    // Variables para llevar el conteo de la moda
     int moda = lista[0];
     int maxCuenta = 1;
     int cuentaActual = 1;
 
-    // Recorrer la lista para encontrar la moda con el count
     for (int i = 1; i < lista.Count; i++)
     {
         if (lista[i] == lista[i - 1])
         {
-            // Incrementar la cuenta si el número es el mismo que el anterior
-            cuentaActual =+1;
+            cuentaActual++;
         }
         else
         {
-            // Resetear la cuenta si el número es diferente
             cuentaActual = 1;
         }
 
-        // Actualizar la moda si encontramos una frecuencia mayor
         if (cuentaActual > maxCuenta)
         {
             maxCuenta = cuentaActual;
@@ -60,22 +112,31 @@ static int CalcularModa(List<int> lista)
     return moda;
 }
 
-
-static double Calcular_Media (List<int> lista) {
+static double CalcularMediana(List<int> lista)
+{
+    lista.Sort();
+    int n = lista.Count;
+    if (n % 2 == 0)
     {
-        lista.Sort();
-        int n = lista.Count;
-        if (n % 2 == 0)
-        {
-            return (lista[n / 2 - 1] + lista[n / 2 ]) / 2;
-        }
-        else
-        {
-            return lista[n / 2];
-        }
-
-
+        return (lista[n / 2 - 1] + lista[n / 2]) / 2.0;
+    }
+    else
+    {
+        return lista[n / 2];
     }
 }
 
-    
+static double CalcularDesviacion(List<int> lista)
+{
+    double promedio = lista.Average();
+    double sumaDesviacion = 0;
+
+    foreach (int numero in lista)
+    {
+        double diferencia = numero - promedio;
+        sumaDesviacion += diferencia * diferencia;
+    }
+
+    double varianza = sumaDesviacion / lista.Count;
+    return Math.Sqrt(varianza);
+}
